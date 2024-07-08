5d067c9b289de6fea98ee0921660dc08aa0a9df2
Anne Nguyen <baitoanngoisaodo@gmail.com>
2024-07-08 17:32:12 -0300

    created 2 endpoints with go fiber

---
+++ go_fiber/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+type Ninja struct {
+	Name   string
+	Weapon string
+}
+
+func getNinja(ctx *fiber.Ctx) error {
+	return ctx.Status(fiber.StatusOK).JSON(ninja)
+}
+
+var ninja Ninja
+
+func createNinja(ctx *fiber.Ctx) error {
+	body := new(Ninja)
+	err := ctx.BodyParser(body)
+	if err != nil {
+		ctx.Status(fiber.StatusBadRequest).SendString(err.Error())
+		return err
+	}
+
+	ninja = Ninja{
+		Name:   body.Name,
+		Weapon: body.Weapon,
+	}
+
+	return ctx.Status(fiber.StatusOK).JSON(ninja)
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	ninjaApp := app.Group("/ninja")
+	ninjaApp.Get("", getNinja)
+	ninjaApp.Post("", createNinja)
+
+	app.Listen(":3000")
+}
